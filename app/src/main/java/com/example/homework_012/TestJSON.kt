91a817923e98742a87431ceae395bd1a546d8269
d-ava <d.avalishvili@gmail.com>
2021-10-12 20:41:51 +0400

    JSON from https://run.mocky.io/v3/3588163c-a9c8-488c-af9a-534b392e7128  has been parsed :)
    data class TestJSON

--- app/src/main/java/com/example/homework_012/TestJSON.kt
+++ app/src/main/java/com/example/homework_012/TestJSON.kt
@@ -1,6 +1,5 @@
 package com.example.homework_012
 
-import androidx.annotation.StringRes
 import com.google.gson.annotations.SerializedName
 
 data class TestJSON(
@@ -12,8 +11,7 @@ data class TestJSON(
     val requestedBy: String,
     val acceptedBy: String? = null,
     val author: String,
-    @SerializedName("category")
-    val category: String,               //in case want to change name from JSON
+    val category: String,
     val locations: Locations,
     val filters: List<Filters>,
     val type: String?,
@@ -50,7 +48,7 @@ data class TestJSON(
         val startDateOption: String? = null,
         val endDateOption: String? = null,
         val pickUpLocation: PickUpLocation,
-        val deliveryLocation: DeliveryLocation
+        val deliveryLocation: DeliveryLocation,
         val provider: String?,
         val pickUpLocationAddress: String?,
         val deliveryLocationAddress: String?,
@@ -73,8 +71,106 @@ data class TestJSON(
         val modelID: String,
         val brandID: String,
         val year: Int,
-        val specifications: List<Specifications>
+        val specifications: List<Specifications>,
+        val weight: Int,
+        @SerializedName("additional_specifications")
+        val additionalSpecifications: String? = null,
+        val structureId: String,
+        val organizationId: String,
+        val beaconType: String? = null,
+        val beaconVendor: String,
+        val RFID: String,
+        val dailyPrice: String? = null,
+        val inactive: String? = null,
+        val tag: Tag,
+        val telematicBox: String? = null,
+        val createdAt: String,
+        @SerializedName("special_number")
+        val specialNumber: String? = null,
+        @SerializedName("last_check")
+        val lastCheck: String,
+        @SerializedName("next_check")
+        val nextCheck: String,
+        @SerializedName("responsible_person")
+        val responsiblePerson: String? = null,
+        @SerializedName("test_type")
+        val testType: String? = null,
+        @SerializedName("unique_equipment_id")
+        val uniqueEquipmentId: String,
+        @SerializedName("bgl_number")
+        val bglNumber: String,
+        @SerializedName("serial_number")
+        val serialNumber: String? = null,
+        val inventory: String? = null,
+        val warehouseId: String,
+        val trackingTag: String? = null,
+        val workingHours: String,
+        @SerializedName("navaris_criteria")
+        val navarisCriteria: String? = null,
+        @SerializedName("dont_send_to_as400")
+        val dontSendToAs400: Boolean,
+        val model: Model,
+        val brand: Brand,
+        val category: Category,
+        val structure: Structure,
+        val wareHouse: String? = null,
+        val equipmentMedia: List<EquipmentMedia>,
+        val telematics: List<Telematics>,
+        val isMoving: Boolean
 
     )
-    data class Specifications()
+
+    data class Specifications(val key: String, val value: Any)
+    data class Tag(val date: String, val authorise: String, val media: List<String>?)
+    data class Model(
+        val id: String,
+        val name: String,
+        val createdAt: String,
+        val brand: Brand,
+
+        )
+
+    data class Brand(val id: String, val name: String, val createdAt: String)
+    data class Category(
+        val id: String,
+        val name: String,
+        @SerializedName("name_de")
+        val nameDe: String,
+        val createdAt: String,
+        val media: List<String>?
+    )
+
+    data class Structure(
+        val id: String,
+        val name: String,
+        val type: String,
+        val color: String
+    )
+
+    data class EquipmentMedia(
+        val id: String,
+        val name: String,
+        val files: List<SizePath>
+    )
+
+    data class SizePath(val size: String, val path: String)
+    data class Telematics(
+        val timestamp: Long,
+        val eventType: String,
+        val projectId:String,
+        val equipmentId:String,
+        val locationName: String,
+        val location: Location,
+        val costCenter:String,
+        val lastLatitude: Double,
+        val lastLongitude: Double,
+        val lastLatLonPrecise: Boolean,
+        val lastAddressByLatLon:String
+
+    )
+    data class Location(
+        val type:String,
+        val coordinates : List<List<List<List<Double>>>>,
+
+    )
 }
